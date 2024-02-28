cb0d889ffc7663b3ecda594c67f51139f03509c2
frankyz21 <masoodah.ba@gmail.com>
2024-02-28 17:16:00 +0500

    twilio integrated and sending real time sms to on new user sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  after_action :send_sms_on_sign_up, only: [:create] if controller_path.eql?("devise/regesteration")
+
+  private
+
+  def send_sms_on_sign_up
+    TwilioService.new.send_sms(current_user.phone_number, "Welcome to our application! Thanks for signing up.")
+  end
 end
