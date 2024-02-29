fd5a188401fe891da392d4488dc9d06af823e50f
frankyz21 <masoodah.ba@gmail.com>
2024-02-29 16:50:32 +0500

    validations in model

--- app/models/sms_notification.rb
+++ app/models/sms_notification.rb
@@ -5,7 +5,7 @@ class SmsNotification < ApplicationRecord
 
   def send_sms
     response = TwilioService.new.send_sms(self.recipient_phone_number, self.message)
-    response.include?("error") ? error = response : error = response.error_code
+    response.class.eql?(String) && response.include?("error") ? error = response : error = response.error_code
     self.update(status: error.nil? ? "sent" : "failed", error_message: error)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\+?\d{10,}\z/, message: "should be a valid phone number" }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "should be a valid email address" }
 end
